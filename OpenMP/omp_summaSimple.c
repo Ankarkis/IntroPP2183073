09cbb32cbf23d7771cd82d8aaa8140093bed0672
Carlos Castañeda <83093835+Ankarkis@users.noreply.github.com>
2023-04-21 19:42:17 -0500

    Update omp_summaSimple.c

--- OpenMP/omp_summaSimple.c
+++ OpenMP/omp_summaSimple.c
@@ -7,7 +7,11 @@ int main()
 
     printf("Enter a positive integer: ");
     scanf("%d", &num);
-
+/*En este bloque de código se utiliza la directiva pragma omp para crear un equipo de hilos y distribuir el trabajo del bucle for en paralelo. 
+La cláusula parallel indica que el siguiente bloque de código se ejecutará en paralelo. 
+La cláusula for indica que se debe distribuir el trabajo del bucle for entre los hilos creados. 
+La cláusula reduction indica que la variable sum debe ser compartida entre los hilos, y que cada hilo debe mantener una copia privada de la variable. 
+Al final del bucle, los valores de todas las copias privadas se suman y se almacenan en la variable sum.*/
     #pragma omp parallel for reduction(+:sum)
     for(count = 1; count <= num; ++count)
     {
